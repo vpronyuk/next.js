27b67c50102ea6c2fe2aee476c0affbc16b23432
Volodymyr Proniuk <vpronyuk@gmail.com>
2024-09-29 23:07:09 +0300

    add dashboard & settings pages

---
+++ src/app/dashboard/_settings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export interface PageProps {}
+
+export default function Page({}: PageProps) {
+  return (
+    <main>
+      <h1 className="text-xl">Settings Page</h1>
+    </main>
+  );
+}
