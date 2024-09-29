f7cd9365ee74818ecbc9addb46a482e8014fd29c
Volodymyr Proniuk <vpronyuk@gmail.com>
2024-09-29 20:26:06 +0300

    usage global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import StatusLabel, { Status } from './components/status-label';
-
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Home Page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <h1>Home Page</h1>
     </main>
   );
 }
