0eeab997964f4e8815715ae245dcb8758c2d43a0
Volodymyr Proniuk <vpronyuk@gmail.com>
2024-09-29 20:20:37 +0300

    add tailwind + clsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './globals.css';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body> {children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import StatusLabel, { Status } from './components/status-label';
+
 export default function Home() {
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1 className="text-xl">Home Page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
