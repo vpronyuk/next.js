70f15a74ca6879b69564a7afe69b508cc6bd867d
Volodymyr Proniuk <vpronyuk@gmail.com>
2024-09-30 21:07:35 +0300

    add layout

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from '../components/header';
-import Toolbar from '../components/toolbar';
-import SearchInput from '../components/search-input';
-import AddCompanyButton from '../components/add-company-button';
-import CompanyTable from '../components/company-table';
-import CompanyRow from '../components/company-row';
-import { Status } from '../components/status-label';
+import Header from '../../components/header';
+import Toolbar from '../../components/toolbar';
+import SearchInput from '../../components/search-input';
+import AddCompanyButton from '../../components/add-company-button';
+import CompanyTable from '../../components/company-table';
+import CompanyRow from '../../components/company-row';
+import { Status } from '../../components/status-label';
 
 export interface PageProps {}
 
