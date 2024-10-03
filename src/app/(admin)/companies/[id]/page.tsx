78ef47471d3375d9c7b3bedf3c1bea4a298deef9
Volodymyr Proniuk <vpronyuk@gmail.com>
2024-10-03 23:08:24 +0300

    errors

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -5,15 +5,10 @@ export interface PageProps {
   params: { id: string };
 }
 
-export function generateStaticParams() {
-  return [{ id: '1' }, { id: '2' }, { id: '3' }];
-}
-
 export default function Page({ params }: PageProps) {
   return (
     <>
-      <Header>Companies ({params.id})</Header>
-      <p>{new Date().toTimeString()}</p>
+      <Header>Company ({params.id})</Header>
     </>
   );
 }
