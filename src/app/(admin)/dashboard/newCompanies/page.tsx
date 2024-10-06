698cee09038d3d1e56907c553efa7663816f1fe4
Volodymyr Proniuk <vpronyuk@gmail.com>
2024-10-06 19:22:32 +0300

    add parallel routing

--- src/app/(admin)/dashboard/newCompanies/page.tsx
+++ src/app/(admin)/dashboard/newCompanies/page.tsx
@@ -6,7 +6,8 @@ export interface PageProps {}
 export default function Page({}: PageProps) {
   return (
     <>
-      <Header>Dashboard</Header>
+      <Header>Dashboard / New companies</Header>
+      <div>Some additional info</div>
     </>
   );
 }
---
+++ src/app/components/stat-card/index.ts
@@ -0,0 +1,5 @@
+import StatCard from '../stat-card/stat-card';
+
+export * from '../stat-card/stat-card';
+
+export default StatCard;
